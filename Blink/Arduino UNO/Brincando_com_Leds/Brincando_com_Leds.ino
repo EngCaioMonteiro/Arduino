2d1e35b6d6026add0bd5933129149f6cb1abac9f
Caio Oliveira Monteiro <caio.monteiro@grupoicts.com.br>
2021-08-31 13:25:51 -0400

    Brincando com LEDs

---
+++ Blink/Arduino UNO/Brincando_com_Leds/Brincando_com_Leds.ino
@@ -0,0 +1,70 @@
+void setup () {
+    pinMode(1, OUTPUT);
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+    pinMode(10, OUTPUT);
+    pinMode(11, OUTPUT);
+    pinMode(12, OUTPUT);
+    pinMode(13, OUTPUT);
+}
+
+int tempo = 200;
+
+void loop () {
+    digitalWrite(13,1);
+    digitalWrite(2,1);
+    delay(tempo);
+    digitalWrite(13,0);
+    digitalWrite(2,0);
+    digitalWrite(12,1);
+    digitalWrite(3,1);
+    delay(tempo);
+    digitalWrite(12,0);
+    digitalWrite(3,0);
+    digitalWrite(11,1);
+    digitalWrite(4,1);
+    delay(tempo);
+    digitalWrite(11,0);
+    digitalWrite(4,0);
+    digitalWrite(10,1);
+    digitalWrite(5,1);
+    delay(tempo);
+    digitalWrite(10,0);
+    digitalWrite(5,0);
+    digitalWrite(9,1);
+    digitalWrite(6,1);
+    delay(tempo);
+    digitalWrite(9,0);
+    digitalWrite(6,0);
+    digitalWrite(8,1);
+    digitalWrite(7,1);
+    delay(tempo);
+    digitalWrite(8,0);
+    digitalWrite(7,0);
+    digitalWrite(9,1);
+    digitalWrite(6,1);
+    delay(tempo);
+    digitalWrite(9,0);
+    digitalWrite(6,0);
+    digitalWrite(10,1);
+    digitalWrite(5,1);
+    delay(tempo);
+    digitalWrite(10,0);
+    digitalWrite(5,0);
+    digitalWrite(11,1);
+    digitalWrite(4,1);
+    delay(tempo);
+    digitalWrite(11,0);
+    digitalWrite(4,0);
+    digitalWrite(12,1);
+    digitalWrite(3,1);
+    delay(tempo);
+    digitalWrite(12,0);
+    digitalWrite(3,0);
+}
