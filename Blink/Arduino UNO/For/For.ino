73a1f69bd59e19a2b0b47bb9cf78968b9d9b97d1
Caio Oliveira Monteiro <caio.monteiro@grupoicts.com.br>
2021-09-01 19:32:21 -0400

    Atualização do For

--- Blink/Arduino UNO/For/For.ino
+++ Blink/Arduino UNO/For/For.ino
@@ -1,12 +1,19 @@
 int pinLed[9] = {12,11,10,9,8,7,6,5,4};
+int numeroLed;
 
 void setup() {
 
-  for {x = 0}; x <= 9; x = x + 1 {
-  pinMode{pinLED[x], OUTPUT}
+  int x;
+
+  for {x = 0}; x <= 8; x = x + 1 {
+  pinMode{pinLed[x], OUTPUT}
   }
 }
 
 void loop() {
- 
+
+  for {numeroLed = 0}; numeroLed <= 8; numeroLed = numeroLed + 1 {
+  digitalWrite(numeroLed, HIGH);
+  delay(200);
+  }
 }
